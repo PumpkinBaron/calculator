dd2c1c9d2a2ac90480abe954369058254bb02b05
PumpkinBaron <mutt-tableware-yam@duck.com>
2026-05-15 16:11:04 -0500

    Correct javascript logic
    Various syntax errors are fixed.
    
    A new helper function compute() is added that accepts two numbers
    and return the value of the computation based on the current value
    value of the operator.
    
    IDs that start with a symbol or letter cannot be queried. Therefore
    the buttons now have an ID of "b[input value]". The b is removed
    before it is passed to clickButton().
    
    clickButton() is structured in reverse order. This ensures that
    multiple cases are not fired, like pressing 2 on a calculator and
    setting firstNum and secondNum to 2 at the same time.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 /// Variables
+const display = document.querySelector(".display-contents");
+const buttons = document.querySelector(".buttons");
 /// "Enter" corresponds to "="
-const operators = ["+", "-", "*", "/", "Enter"]
+const operators = ["+", "-", "*", "/"]
 const numbers = ["0", "1", "2", "3",
                  "4", "5", "6", "7",
                  "8", "9"]
 
-let screenValue = "";
 let firstNum = "";
 let secondNum = "";
 let operator = "";
@@ -17,23 +18,33 @@ let secondNumDecimalFlag = false;
 /// Boolean checks
 /// Checks if the keyboard input corresponds to a calculator button
 function isButton(keyboardInput){
-    return operators.contains(keyboardInput)
-        || numbers.contains(keyboardInput)
-        || keyboardInput === ".";
+    return operators.includes(keyboardInput)
+        || numbers.includes(keyboardInput)
+        || keyboardInput === "."
+        || keyboardInput === "Delete"
+        || keyboardInput === "Backspace"
+        || keyboardInput === "Enter";
 }
 
 function isOperator(buttonInput){
-    return operators.contains(buttonInput);
+    return operators.includes(buttonInput);
 }
 
 function isANumberOrDecimalPoint(string){
-    return numbers.contains(string) || string === ".";
+    return numbers.includes(string) || string === ".";
 }
 
 // Sub-operations
 
+function compute(a,b){
+    if (operator === "+") return a + b;
+    if (operator === "-") return a - b;
+    if (operator === "*") return a * b;
+    if (operator === "/") return a / b;
+}
+
+
 function clear(){
-    screenValue = "";
     firstNum = "";
     secondNum = "";
     operator = "";
@@ -50,41 +61,30 @@ function backspace(string){
 function operate(){
     const a = Number(firstNum);
     const b = Number(secondNum);
+    console.log(`A is ${a}, operator is ${operator}, and b is ${b}.`)
+    let result = compute(a , b);
 
-    let results = "";
-
-    switch(operator)  {
-        case "+": {
-            results = a + b;
-        }
-        case "-": {
-            results = a - b;
-        }
-        case "*": {
-            results = a * b;
-        }
-        case "/": {
-            results = a / b;
-        }
-    }
-    if (!isInteger(results)){
-    results = results.toFixed(5);
-    }
-    if (number > 1000000000){
-        results = results.toExponential();
+    console.log(`Result is now ${result}. Fixing to 5th decimal place...`)
+    result = Number(result.toFixed(5));
+    console.log(`Result is now ${result}.`)
+    if (result > 1000000000){
+        result = result.toExponential();
     }
     clear();
-    firstNum = results + "";
+    firstNum = result + "";
 }
 
 /// Main Function
 function clickButton(buttonInput){
+    console.log(`Received input ${buttonInput}.`)
     // Operations
-    if (buttonInput === "clear"){
+    if (buttonInput === "Clear"){
+        console.log("Clearing...")
          clear();
     }
     // "Enter" corresponds to "=".
     if (buttonInput === "Enter" && operator !== "" && secondNum !==  "") {
+        console.log("Computing...")
         if (operator === "/" && Number(secondNum) === 0) {
             clear();
             firstNum = "Cannot divide by 0";
@@ -94,7 +94,8 @@ function clickButton(buttonInput){
     }
     // Remove the last value, if any, from either firstNum, operator,
     // or secondNum in a way that feels like using a text editor.
-    if (buttonInput === "Backspace") {
+    if (buttonInput === "Delete") {
+        console.log("Backspace...")
         if (secondNum === "" && operator === "") {
             firstNum = backspace(firstNum);
         } else if (firstNum !== "" && secondNum === ""){
@@ -105,44 +106,70 @@ function clickButton(buttonInput){
     }
     // Call operate() by inputting an operator into the full calculator
     if (isOperator(buttonInput) && secondNum !== "") {
+        console.log("Computing via operator inputted to a full calculator...")
         operate();
         operator = buttonInput;
     }
-    // Defining first number
-    if (firstNum === "" && isANumberOrDecimalPoint(buttonInput)){
-        if (buttonInput === "." && firstNumDecimalFlag === false) {
-            firstNum += buttonInput;
-            firstNumDecimalFlag = true;
-        } else {
-            firstNum = firstNum + buttonInput;
-        }
-    }
-    // Defining the operator
-    if (isOperator(buttonInput) && firstNum !== "" && operator === ""){
-            operator = buttonInput;
-    }
-    // Defining the second number
-    if (secondNum === "" && isANumberOrDecimalPoint(buttonInput)){
+        // Defining the second number
+    if (operator !== ""  
+        && isANumberOrDecimalPoint(buttonInput)){
+        console.log(`First number is ${firstNum}, which is not an empty string 
+            ${firstNum !== ""} and Second number is ${secondNum} 
+            and button input is ${isANumberOrDecimalPoint(buttonInput)} 
+            Defining the second number...`);
         if (buttonInput === "." && secondNumDecimalFlag === false) {
             secondNum += buttonInput;
             secondNumDecimalFlag = true;
         } else {
             secondNum += buttonInput;
         }
+        console.log(`Second number is now ${secondNum}`);
+    }    
+    // Defining the operator
+    if (isOperator(buttonInput) && firstNum !== "" && secondNum === ""){
+        console.log(`Setting operator to ${buttonInput}...`)
+            operator = buttonInput;
     }
+
+    // Defining first number
+    if (operator === "" 
+        && isANumberOrDecimalPoint(buttonInput)){
+            const newNum = firstNum + buttonInput;
+        console.log(`First number is ${firstNum} 
+            and button input is ${isANumberOrDecimalPoint(buttonInput)} 
+            Defining the first number as ${newNum}...`);
+        if (buttonInput === "." && firstNumDecimalFlag === false) {
+            firstNum += buttonInput;
+            firstNumDecimalFlag = true;
+        } else {
+            firstNum = newNum;
+        }
+        console.log(`First number is now ${firstNum}`);
+    }
+
+    console.log(`Current text content is ${display}. Redefining...`);
+    display.textContent = `${firstNum} ${operator} ${secondNum}`;
+    console.log(`Display set to ${display.textContent}`);
 };
 
 // Listen for button presses, either on screen or on the user's keyboard.
-document.addEventListener('click', target => {
-    if (target.matches('button')) {
-        clickButton(target.id);
-    }
+function getID(element) {
+    return element.id;
+}
+
+buttons.addEventListener('click', event => {
+        console.log(`Registered click on ${event.target.id}`);
+        if (event.target.id !== undefined ){
+        clickButton(event.target.id.slice(1));
+        }
 });
 
-document.addEventListener('keypress', target => {
-    if (isButton(target.key)) {
-        clickButton(target.key);
-    } else if (KeyboardEvent.shiftKey && target.key === "Backspace"){
+document.addEventListener('keypress', event => {
+    console.log(`Registered keyboard input ${event.key}`);
+    if (isButton(event.key)) {
+        clickButton(event.key);
+    } else if (event.shiftKey && event.key === "Backspace"){
         clickButton("clear");
     }
 });
+
